212941d7bf25951e3b1bfc8f3c2ad96cc6134697
athenalin11 <athenalinwork@gmail.com>
2025-11-03 02:00:13 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/',
+  base: '/lotus-bf-frontend-athenalin11/',  // GitHub Pages 倉庫路徑
 
   plugins: [react()],
   resolve: {
